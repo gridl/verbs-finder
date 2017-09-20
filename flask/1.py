f7d2ede4d953710c720da67b63fd1e4252a7c551
Yuriy Dotsya <lovehardgame@yandex.ru>
2017-09-20 09:57:40 +0300

    initial commit

---
+++ flask/1.py
@@ -0,0 +1,10 @@
+def go_play(_list):
+    """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
+    return sum([list(item) for item in _list], [])
+
+def obtain(_list):
+    """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
+    return sum([list(item) for item in _list], [])
+
+def obtain_more(list):
+    print(list)
---
+++ flask/2.py
@@ -0,0 +1,7 @@
+def obtain(_list):
+    """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
+    return sum([list(item) for item in _list], [])
+
+def permit_other(_list):
+    """ [(1,2), (3,4)] -> [1, 2, 3, 4]"""
+    return sum([list(item) for item in _list], [])
